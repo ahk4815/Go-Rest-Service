95c47badab74d808bf2051f0f89e81b13d904b9a
ahk4815 <ahk4815@gmail.com>
2023-06-17 22:59:12 +0530

    Comments - function comments were added

--- Controller/EmployeeController.go
+++ Controller/EmployeeController.go
@@ -8,18 +8,27 @@ import (
 	EmployeeModel "github.com/ahk4815/Go-Rest-Service/Model"
 )
 
+/*
+In-Memory array of Employee records.
+*/
 var employees = []EmployeeModel.Employee{
 	{ID: 1, Name: "John", Designation: "CEO"},
 	{ID: 2, Name: "David", Designation: "CTO"},
 	{ID: 3, Name: "Mary", Designation: "CFO"},
 }
 
+/*
+Returns the list of all the employees in the system.
+*/
 func GetAllEmployees(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Providing all employees - ")
 	json.NewEncoder(w).Encode(employees)
 }
 
+/*
+Returns a welcome message to the system.
+*/
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the Employee Tracking system!")
 	fmt.Println("Endpoint Hit: homePage")
 }
--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 	EmployeeController "github.com/ahk4815/Go-Rest-Service/Controller"
 )
 
+/*
+This function creates a http router and binds the functions
+to the url endpoints for the web service.
+*/
 func handleRequests() {
 	http.HandleFunc("/", EmployeeController.HomePage)
 	http.HandleFunc("/employee", EmployeeController.GetAllEmployees)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
+/*
+Main function.
+*/
 func main() {
 	handleRequests()
 }
